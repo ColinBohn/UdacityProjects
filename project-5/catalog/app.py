35620ec64ae8e71e8edfdedd10e7c0e2030be9f4
ColinBohn <colinbohn@gmail.com>
2017-03-29 23:04:59 -0700

    Initial commit of project 5
    TODO: Item delete

---
+++ project-5/catalog/app.py
@@ -0,0 +1,157 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify,\
+     flash, session as login_session
+from sqlalchemy import create_engine, desc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, User, Category, Item
+from jinja2 import Environment, PackageLoader
+from oauth2client import client, crypt
+from functools import wraps
+
+
+app = Flask(__name__)
+app.secret_key = "VDQx8BEDn7clDzn8I80WH1Mp2apV69qL"
+engine = create_engine('sqlite:///database.sqlite')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+google_client_id = "702305718162-pkoufeo9l51m6rj29sed66h7hbtmm5c0.apps."\
+                   "googleusercontent.com"
+google_secret = "NZLo8q-nVfu-vsm-6g-ApXvK"
+
+
+def app_template(tmpl_name, **kwargs):
+    return render_template(tmpl_name, gcid=google_client_id,
+                           logged_in=login_session.get('id') is not None,
+                           **kwargs)
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if login_session.get('id') is None:
+            return redirect(url_for('showLogin'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+@app.route('/')
+def index():
+    """Lists the latest items in our catalog"""
+    categories = session.query(Category).all()
+    items = session.query(Item).order_by(Item.id.desc()).limit(10).all()
+    return app_template('index.html', categories=categories, items=items)
+
+
+@app.route('/login')
+def showLogin():
+    """Displays login button"""
+    return app_template('login.html')
+
+
+@app.route('/oauth2callback', methods=['POST'])
+def processLogin():
+    """Handles callback from Google for OAuth2"""
+    try:
+        idinfo = client.verify_id_token(request.form["idtoken"],
+                                        google_client_id)
+        if idinfo['iss'] not in ['accounts.google.com',
+                                 'https://accounts.google.com']:
+            raise crypt.AppIdentityError("Wrong issuer.")
+    except crypt.AppIdentityError:
+        return "Authentication error."
+    login_session['id'] = idinfo['sub']
+    return "valid"
+
+
+@app.route('/logout')
+def processLogout():
+    """Displays login button"""
+    login_session['id'] = None
+    return redirect(url_for('index'))
+
+
+@app.route('/catalog/<string:category_name>/items')
+def showCategory(category_name):
+    """Returns a list of the items within a category"""
+    category = session.query(Category).filter_by(name=category_name).one()
+    categories = session.query(Category).all()
+    items = session.query(Item).filter_by(category_id=category.id).all()
+    return app_template('category.html', categories=categories,
+                        category=category, items=items)
+
+
+@app.route('/catalog/<string:category_name>/<string:item_name>')
+def showItem(category_name, item_name):
+    """Returns the description of an item"""
+    item = session.query(Item).filter_by(name=item_name).one()
+    return app_template('item.html', item=item)
+
+
+@app.route('/new/item/')
+@login_required
+def newItemForm():
+    """Displays form to create a new item"""
+    categories = session.query(Category).all()
+    return app_template('new_item.html', categories=categories)
+
+
+@app.route('/new/item/submit', methods=['POST'])
+@login_required
+def newItem():
+    """Receives POST for new item"""
+    item = Item(name=request.form["name"],
+                description=request.form["description"],
+                category_id=request.form["category"])
+    session.add(item)
+    session.flush()
+    flash('Item successfully added!', 'success')
+    return redirect(url_for('index'))
+
+
+@app.route('/edit/item/<string:item_name>')
+@login_required
+def editItemForm(item_name):
+    """Displays form to edit item"""
+    item = session.query(Item).filter_by(name=item_name).one()
+    categories = session.query(Category).all()
+    return app_template('edit_item.html', item=item, categories=categories)
+
+
+@app.route('/edit/item/<string:item_name>/submit', methods=['POST'])
+@login_required
+def editItem(item_name):
+    """Receives POST for edit item"""
+    item = session.query(Item).filter_by(name=item_name).one()
+    item.name = request.form["name"]
+    item.description = request.form["description"]
+    item.category_id = request.form["category"]
+    session.flush()
+    flash('Item successfully edited!', 'success')
+    return redirect(url_for('index'))
+
+
+@app.route('/delete/item/<string:item_name>')
+@login_required
+def deleteItemForm(item_name):
+    """Displays a confirmation to delete an item"""
+    # TODO
+    return None
+
+
+@app.route('/delete/item/<string:item_name>/submit')
+@login_required
+def deleteItem(item_name):
+    """Receives POST to delete item"""
+    # TODO
+    return None
+
+
+@app.route('/catalog.json')
+def json():
+    """JSON dump of the catalog."""
+    categories = session.query(Category).all()
+    return jsonify(Categories=[category.serialize for category in categories])
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=80)
---
+++ project-5/catalog/database_setup.py
@@ -0,0 +1,54 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False, unique=True)
+    items = relationship("Item")
+
+    @property
+    def serialize(self):
+        """ JSON serializer method """
+        return {
+            'id': self.id,
+            'name': self.name,
+            'Items' : [item.serialize for item in self.items]
+
+        }
+
+class Item(Base):
+    __tablename__ = 'items'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False, unique=True)
+    description = Column(String(2000))
+    category_id = Column(Integer, ForeignKey('categories.id'))
+    category = relationship(Category)
+
+    @property
+    def serialize(self):
+        """ JSON serializer """
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'cat_id': self.category_id
+        }
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///database.sqlite')
+    Base.metadata.create_all(engine)
